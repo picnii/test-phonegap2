666ab55afb62691e6afbcd9157181ac62e77f383
picnii <sompop@picnii.me>
2014-05-31 11:54:24 +0700

    Basic Grader

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,13 @@
 function HomeCtrl ($scope) {
 	// body...
-	
+	$scope.is_show_grade = false;
+	$scope.calculate = function()
+	{
+		$scope.is_show_grade = true;
+		if($scope.score == 0)
+			$scope.grade = "F"
+		else
+			$scope.grade = "P";
+
+	}
 }
