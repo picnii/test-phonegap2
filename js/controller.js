9c4c67319aaee9522920dc0cf80b1c6ed5cf92c1
picnii <sompop@picnii.me>
2014-05-31 13:14:56 +0700

    normal grader

--- js/controller.js
+++ js/controller.js
@@ -4,10 +4,17 @@ function HomeCtrl ($scope) {
 	$scope.calculate = function()
 	{
 		$scope.is_show_grade = true;
-		if($scope.score == 0)
-			$scope.grade = "F"
+		if($scope.score > 100 || $scope.score < 0)
+			$scope.grade = "Impossible"
+		else if($scope.score >= 80)
+			$scope.grade = "A";
+		else if($scope.score >= 70)
+			$scope.grade = "B";
+		else if($scope.score >= 60)
+			$scope.grade = "C";
+		else if($scope.score >= 50)
+			$scope.grade = "D";
 		else
-			$scope.grade = "P";
-
+			$scope.grade = "F";
 	}
 }
