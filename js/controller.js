9fa320622230acb25aaea66f1aeb04af5032ae4d
picnii <sompop@picnii.me>
2014-06-01 13:16:07 +0700

    initCtrl

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,27 @@
-function HomeCtrl ($scope, $rootScope, $location) {
-	// body...
-	$scope.question_index = 0;
-	$scope.questions = [
+function initCtrl($rootScope, $location)
+{
+	$rootScope.question_index = 0;
+	$rootScope.questions = [
 		{img: "http://www.siamdara.com/Picture_Girl/081203C1S4426.jpg", answer:"แกงส้ม" },
 		{img: "http://1.bp.blogspot.com/-KEhWO3pJPzU/T2HE7S3sinI/AAAAAAAAABQ/aSzEQChfaps/s1600/%25E0%25B8%25AA%25E0%25B9%2589%25E0%25B8%25A1%25E0%25B8%2595%25E0%25B8%25B3%5B2%5D.jpg", answer:"ส้มตำ" },
 		{img: "http://admin.ladytips.com/Images/News/images/2012/May/24/%E0%B8%95%E0%B8%B8%E0%B9%8A%E0%B8%81%E0%B8%95%E0%B8%B2/%E0%B9%80%E0%B8%AD%E0%B8%A3%E0%B8%B2%E0%B8%A7%E0%B8%B1%E0%B8%932.jpg", answer:"น้ำตก" },
 		{img: "http://topicstock.pantip.com/food/topicstock/2010/11/D9869164/D9869164-0.jpg", answer:"ลาบ" },
 		{img: "http://www.ezythaicooking.com/images/rf2-stir-fried-chinese-kale-with-oyster-sauce-2.jpg", answer:"ผัดคะน้า" }
 	]
+	$rootScope.is_win = false;
+	$rootScope.high_score = 0;
+	$rootScope.high_score_name = "";
+	$rootScope.checkWin = function()
+	{
+		if(!$rootScope.is_win)
+			$location.path('/');
+	}
+}
+app.run(initCtrl);
+
+function HomeCtrl ($scope, $rootScope, $location) {
+	// body...
+	$scope.question_index = 0;
 
 	$scope.img = $scope.questions[$scope.question_index].img;
 	$scope.score = 0;
@@ -29,22 +43,22 @@ function HomeCtrl ($scope, $rootScope, $location) {
 		if($scope.question_index == $scope.questions.length)
 		{
 			$rootScope.is_win = true;
-			if(typeof $rootScope.high_score == 'undefined' || $rootScope.high_score < $scope.score)
+			if($rootScope.high_score < $scope.score)
 			{
 				$rootScope.is_new_record = true;
 				$rootScope.high_score = $scope.score;
 			}
 			$rootScope.current_score = $scope.score;
-			$location.path('/win');
+			$location.path('/win');	
 		}
+		
 	}
 
 }
 
 function WinCtrl($scope, $location, $rootScope)
 {
-	if(!$scope.is_win)
-		$location.path('/');
+	$scope.checkWin();
 	$scope.sendScore = function()
 	{
 		$rootScope.high_score_name = $scope.name;
